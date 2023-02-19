4da1838177fc5a6fe1ac8fc2721a68a3ea74ac9c
WJQ <1092229612@qq.com>
2023-02-19 13:39:02 +0800

    完成了发送验证码基础功能

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,49 +1,53 @@
-import { request } from "../utils/request";
+import { request } from '../utils/request'
 
 // 注册
 export function Reguser(data: any) {
-
-    return request({
-        method: 'post',
-        url: <string>'/user/reguser',
-        data,
-    })
+  return request({
+    method: 'post',
+    url: <string>'/user/reguser',
+    data,
+  })
 }
 
 // 登录
 export function Login(data: any) {
-
-    return request({
-        method: 'post',
-        url: <string>'/user/login',
-        data,
-    })
+  return request({
+    method: 'post',
+    url: <string>'/user/login',
+    data,
+  })
 }
 
-// 获取验证码
+// 获取图形验证码
 export function getCaptacha() {
+  return request({
+    method: 'get',
+    url: <string>'/user/captcha',
+    // responseType: 'blob'
+  })
+}
 
-    return request({
-        method: 'get',
-        url: <string>'/user/captcha',
-        // responseType: 'blob'
-    })
+// 获取短信验证码
+export function getMsg(data: any) {
+  return request({
+    method: 'post',
+    url: <string>'/user/aliyun/sendSms',
+    data,
+  })
 }
 
 // 获取用户是否登录
 export function isLogin() {
-
-    return request({
-        method: 'get',
-        url: <string>'/user/islogin',
-    })
+  return request({
+    method: 'get',
+    url: <string>'/user/islogin',
+  })
 }
 
 // 退出登录
 export function logOut() {
-
-    return request({
-        method: 'post',
-        url: <string>'/user/logout',
-    })
+  return request({
+    method: 'post',
+    url: <string>'/user/logout',
+  })
 }
--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -2,7 +2,9 @@
   <el-container>
     <el-header>
       <el-menu class="el-menu-demo" mode="horizontal" :ellipsis="false" router>
-        <el-menu-item index="/Home"><img src="../../assets/imgs/logo.png" alt="" /></el-menu-item>
+        <el-menu-item index="/Home"
+          ><img src="../../assets/imgs/logo.png" alt=""
+        /></el-menu-item>
         <el-menu-item index="/Login" style="font-size: 20px">登录</el-menu-item>
         <div class="flex-grow" />
         <el-menu-item index="/Home">首页</el-menu-item>
@@ -13,31 +15,67 @@
       <div>
         <div class="from-box">
           <div class="login-title">账号登录</div>
-          <el-form ref="ruleFormRef" class="login-from" label-position="right" :model="formLabelAlign"
-            style="max-width: 400px" :rules="rules" :hide-required-asterisk="true">
-            <el-form-item label="" prop="username">
-              <el-input size="default" maxlength="18" v-model="formLabelAlign.username" style="height: 50px"
-                placeholder="手机号/邮件地址/帐号名" :prefix-icon="User" />
+          <el-form
+            ref="ruleFormRef"
+            class="login-from"
+            label-position="right"
+            :model="formLabelAlign"
+            style="max-width: 400px"
+            :rules="rules"
+            :hide-required-asterisk="true"
+          >
+            <el-form-item label="" prop="phoneNumber">
+              <el-input
+                size="default"
+                maxlength="18"
+                v-model="formLabelAlign.phoneNumber"
+                style="height: 50px"
+                placeholder="手机号"
+                :prefix-icon="User"
+              />
             </el-form-item>
-            <el-form-item label="" prop="password">
-              <el-input size="default" maxlength="18" type="password" v-model="formLabelAlign.password" show-password
-                style="height: 50px" placeholder="密码" :prefix-icon="Lock" />
+            <el-form-item label="" prop="phoneCode">
+              <el-input
+                size="default"
+                maxlength="18"
+                v-model="formLabelAlign.phoneCode"
+                style="width: 60%; float: left; height: 50px"
+                placeholder="短信验证码"
+                :prefix-icon="Lock"
+              />
+              <el-button
+                type="primary"
+                size="large"
+                class="get-code"
+                bg
+                text
+                @click="getMsg1()"
+                >获取验证码</el-button
+              >
             </el-form-item>
             <el-form-item label="" prop="code">
-              <el-input placeholder="验证码" v-model="formLabelAlign.code" style="width: 40%; float: left; height: 50px"
-                :prefix-icon="Key"></el-input>
+              <el-input
+                placeholder="图形验证码"
+                v-model="formLabelAlign.code"
+                style="width: 60%; float: left; height: 50px"
+                :prefix-icon="Key"
+              ></el-input>
               <div class="code-css" @click="getcode">
-                <img :src="imageUrl" style="width: 120px; margin-left: 20px; height: 50px" alt="code" />
+                <img
+                  :src="imageUrl"
+                  style="width: 120px; margin-left: 20px; height: 50px"
+                  alt="code"
+                />
               </div>
             </el-form-item>
-            <!-- <el-button type="primary" size="large" class="loginbtn" @click="submitForm(ruleFormRef)">注册</el-button> -->
-            <el-button type="primary" size="large" class="loginbtn" @click="submitLoginForm(ruleFormRef)">登录</el-button>
+            <el-button
+              type="primary"
+              size="large"
+              class="loginbtn"
+              @click="submitLoginForm(ruleFormRef)"
+              >登录</el-button
+            >
           </el-form>
-          <div class="operation">
-            <el-link type="info" style="margin-left: 5px" :underline="false" @click="goRegister">注册</el-link>
-            <span>|</span>
-            <el-link type="info" :underline="false" @click="sendMsg">忘记密码</el-link>
-          </div>
         </div>
       </div>
     </el-main>
@@ -46,128 +84,113 @@
 </template>
     
 <script lang="ts" setup>
-import { User, Lock, Key } from '@element-plus/icons-vue'
-import { ElForm, ElFormItem, ElInput, ElButton, ElLoading, ElMessage } from 'element-plus'
-import { useRouter } from 'vue-router'
-import { ref, reactive } from 'vue'
-import { setToken } from '../../utils/auth'
-import  Client  from '../../utils/msg'
-import { Reguser, getCaptacha, Login } from '../../api/User'
-import type { FormInstance, FormRules } from 'element-plus'
+import { User, Lock, Key } from "@element-plus/icons-vue";
+import {
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElButton,
+  ElLoading,
+  ElMessage,
+} from "element-plus";
+import { useRouter } from "vue-router";
+import { ref, reactive } from "vue";
+import { setToken } from "../../utils/auth";
+import { Reguser, getCaptacha, Login, getMsg } from "../../api/User";
+import type { FormInstance, FormRules } from "element-plus";
+import { log } from "console";
 
-const router = useRouter()
-let codeFlag = ref(true)
-let formLabelAlign = reactive({ username: '', password: '', code: '' })
-let ruleFormRef = ref<FormInstance>()
+const router = useRouter();
+let codeFlag = ref(true);
+let formLabelAlign = reactive({ phoneNumber: "", phoneCode: "", code: "" });
+let ruleFormRef = ref<FormInstance>();
 // 定义一个校验规则
 const rules = reactive<FormRules>({
-  username: [
-    { required: true, message: '请输入手机号/邮件地址/帐号名', trigger: 'blur' },
-    { min: 4, max: 15, message: 'Length should be 4 to 15', trigger: 'blur' }
+  phoneNumber: [
+    {
+      required: true,
+      message: "请输入手机号",
+      trigger: "blur",
+    },
+    { min: 11, max: 11, message: "请输入正确的手机号", trigger: "blur" },
   ],
-  password: [
-    { required: true, message: '请输入密码', trigger: 'blur' },
-    { min: 4, max: 15, message: 'Length should be 4 to 15', trigger: 'blur' }
+  phoneCode: [
+    { required: true, message: "请输入短信验证码", trigger: "blur" },
+    { min: 6, max: 6, message: "请输入正确的短信验证码", trigger: "blur" },
   ],
   code: [
-    { required: true, message: '请输入验证码', trigger: 'blur' },
-    { min: 4, max: 4, message: '请完整输入验证码', trigger: 'blur' }
-  ]
-})
-let loading: any
-let imageUrl: any = ref()
+    { required: true, message: "请输入图形验证码", trigger: "blur" },
+    { min: 4, max: 4, message: "请输入正确的图形验证码", trigger: "blur" },
+  ],
+});
+let loading: any;
+let imageUrl: any = ref();
 // 是否加载动画
-//注册
-const submitForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return
-  await formEl.validate((valid, fields) => {
-    if (valid) {
-      loading = ElLoading.service({
-        lock: true,
-        text: '注册中',
-        background: 'rgba(0, 0, 0, 0.8)'
-      })
-    }
-  })
-  let formData = new FormData()
-  formData.append('username', formLabelAlign.username)
-  formData.append('password', formLabelAlign.password)
-  formData.append('captcha', formLabelAlign.code)
-
-  let data: any = await Reguser(formData)
-  if (data.status == 200) {
-    setTimeout(() => {
-      loading.close()
-      router.replace('/Login')
-      ElMessage({
-        message: `注册成功`,
-        type: 'success'
-      })
-    }, 1000)
-  } else {
-    loading.close()
-    ElMessage({
-      message: `${data.message}`,
-      type: 'warning'
-    })
-  }
-}
 
 //登录
 const submitLoginForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return
+  if (!formEl) return;
   await formEl.validate((valid, fields) => {
     if (valid) {
       loading = ElLoading.service({
         lock: true,
-        text: '登录中',
-        background: 'rgba(0, 0, 0, 0.8)'
-      })
+        text: "登录中",
+        background: "rgba(0, 0, 0, 0.8)",
+      });
     }
-  })
-  let formData = new FormData()
-  formData.append('username', formLabelAlign.username)
-  formData.append('password', formLabelAlign.password)
-  formData.append('captcha', formLabelAlign.code)
+  });
+  let formData = new FormData();
+  formData.append("phoneNumber", formLabelAlign.phoneNumber);
+  formData.append("phoneCode", formLabelAlign.phoneCode);
+  formData.append("captcha", formLabelAlign.code);
 
-  let data: any = await Login(formData)
+  let data: any = await Login(formData);
   if (data.status == 200) {
     setTimeout(() => {
-      loading.close()
-      router.replace('/Home')
+      loading.close();
+      router.replace("/Home");
       ElMessage({
         message: `登录成功`,
-        type: 'success'
-      })
-    }, 1000)
+        type: "success",
+      });
+    }, 1000);
   } else {
-    loading.close()
+    loading.close();
     ElMessage({
       message: `${data.message}`,
-      type: 'warning'
-    })
+      type: "warning",
+    });
   }
-}
+};
 
 async function getcode() {
-  let data: any = await getCaptacha()
-  let blob = new Blob([data]) // 返回的文件流数据
+  let data: any = await getCaptacha();
+  let blob = new Blob([data]); // 返回的文件流数据
   // imageUrl.value = window.URL.createObjectURL(blob) // 将他转化为路径
   // console.log(data)
   // console.log(encodeURIComponent(data))
-  imageUrl.value = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(data)
+  imageUrl.value =
+    "data:image/svg+xml;charset=utf-8," + encodeURIComponent(data);
   // console.log(imageUrl.value)
 }
-getcode()
+getcode();
+
+const getMsg1 = async () => {
+  let code = "";
+  for (var i = 0; i < 6; i++) {
+    code += Math.floor(Math.random() * 10);
+  }
+  let formData = new FormData();
+  formData.append("phoneNumber", formLabelAlign.phoneNumber);
+  formData.append("phoneCode", code);
+  // let data: any = await getMsg(formData);
+  // console.log(data);
+};
 
 const goRegister = () => {
   // router.push()
-  router.push('/Register')
-}
-
-const sendMsg=()=>{
-  Client.main([])
-}
+  router.push("/Register");
+};
 </script>
     
 <style lang="less" scoped>
@@ -177,14 +200,14 @@ const sendMsg=()=>{
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: url('../../assets/imgs/background_login.jpg');
+  background-image: url("../../assets/imgs/background_login.jpg");
   background-repeat: no-repeat;
   background-size: 100% 100%;
 
   .pic {
     width: 658px;
     height: 485px;
-    background-image: url('../../assets/imgs/login.webp');
+    background-image: url("../../assets/imgs/login.webp");
     margin-right: 200px;
   }
 
@@ -256,6 +279,9 @@ const sendMsg=()=>{
     color: rgb(144, 147, 153);
     margin: 5px;
   }
+}
+.get-code {
+  margin-left: 20px;
 }
 </style>
     
